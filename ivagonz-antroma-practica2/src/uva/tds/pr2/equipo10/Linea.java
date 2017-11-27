e048a0a4279218d79574e05925f430d9884e7160
Iván González Rincón <ivan.gonzalez.rincon@alumnos.uva.es>
2017-11-27 18:41:17 +0100

    Añadido javadoc para addParadaIntermedia() de la clase Linea

--- ivagonz-antroma-practica2/src/uva/tds/pr2/equipo10/Linea.java
+++ ivagonz-antroma-practica2/src/uva/tds/pr2/equipo10/Linea.java
@@ -3,39 +3,48 @@ package uva.tds.pr2.equipo10;
 /**
  * Implementacion basica de una linea de buses
  * 
- * @author antroma 
+ * @author antroma
  * @author ivagonz
  *
  */
 public class Linea {
 
 	/**
-	 * Constructor de la clase linea 
+	 * Constructor de la clase linea
 	 * 
-	 * @param identificador Identificadeor de la linea. Debe ser correcto: identificador positivo
-	 * @param paradas Array que contiene las paradas de la linea y cuya primera y ultima posicion seran la primera
-	 *  y ultima parada de la linea respectivamente. Debe ser correcto:
-	 * 		No debe ser nulo
-	 * 		No debe contener elementos nulos
-	 * 		Debe tener 3 elmentos o mas
-	 * 		La distancia entre el primer y ultimo elemento del array debe ser menor a 100
+	 * @param identificador
+	 *            Identificadeor de la linea. Debe ser correcto: identificador
+	 *            positivo
+	 * @param paradas
+	 *            Array que contiene las paradas de la linea y cuya primera y
+	 *            ultima posicion seran la primera y ultima parada de la linea
+	 *            respectivamente. Debe ser correcto: No debe ser nulo No debe
+	 *            contener elementos nulos Debe tener 3 elmentos o mas La
+	 *            distancia entre el primer y ultimo elemento del array debe ser
+	 *            menor a 100.
+	 * @throws IllegalArgumentException
+	 *             en caso de incumplir las condiciones impuestas a los
+	 *             parámetros.
 	 */
-	 public Linea(int identificador, Parada[] paradas) {
+	public Linea(int identificador, Parada[] paradas) {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Parada[] getParadas() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	/**
+	 * Función para añadir Paradas intermedias a la línea.
+	 * 
+	 * @param parada
+	 *            Parada a añadir. Debe ser correcta: No nula.
+	 * @param posicion
+	 *            Posicion en la que añadir la Parada.Debe ser correcta: No
+	 *            puede ser el inicio ni el final de la Linea.
+	 * @throws IllegalArgumentException
+	 *             en caso de incumplir las condiciones impuestas a los
+	 *             parámetros.
+	 */
 	public void addParadaIntermedia(Parada parada, int posicion) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
-
-	
 
 }
