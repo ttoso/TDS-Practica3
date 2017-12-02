507e55af7507d1a44c70ed58098d5c552a0041f2
Iván González Rincón <ivan.gonzalez.rincon@alumnos.uva.es>
2017-12-02 16:38:51 +0100

    Añadido el test inicial del constructor de la clase Red

--- ivagonz-antroma-practica2/src/uva/tds/pr2/equipo10/Red.java
+++ ivagonz-antroma-practica2/src/uva/tds/pr2/equipo10/Red.java
@@ -2,4 +2,18 @@ package uva.tds.pr2.equipo10;
 
 public class Red {
 
+	public Red(Linea[] lineas) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Linea[] getLineas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean hasLineaNull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
