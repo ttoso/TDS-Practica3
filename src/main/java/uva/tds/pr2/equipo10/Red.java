40940dbf593bf7b7738d747aadd7f044c363728d
antroma <antonio.roman@alumnos.uva.es>
2017-12-20 11:36:00 +0100

    Cambio de la raíz del proyecto

---
+++ src/main/java/uva/tds/pr2/equipo10/Red.java
@@ -0,0 +1,125 @@
+package uva.tds.pr2.equipo10;
+
+/**
+ * Implementación sencilla de una red de buses.
+ *
+ * @author ivagonz
+ * @author antroma
+ */
+public class Red {
+
+	/**
+	 * Inicia una red de autobuses con un vector de Lineas que la componen.
+	 *
+	 * @param lineas
+	 *            Vector de lineas de la Red. Debe ser correcto: No nulo, no
+	 *            debe contener elementos nulos y debe contener al menos 2
+	 *            lineas.
+	 * @throws IllegalArgumentException
+	 *             si no se cumplen las conciones impuestas al par�metro.
+	 */
+	public Red(Linea[] lineas) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 *
+	 * @return vector de Lineas de la Red.
+	 */
+	public Linea[] getLineas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Comprueba si la Red tiene alguna Linea nula.
+	 *
+	 * @return true si tiene al menos una Linea nula, false en caso contrario.
+	 */
+	public boolean hasLineaNull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * Agrega una Linea dentro de la Red de buses.
+	 *
+	 * @param linea
+	 *            Linea a agregar en la Red. Debe ser correcta: No nula, no debe
+	 *            estar ya en la Red.
+	 * @throws IllegalArgumentException
+	 *             en caso de incumplir las condiciones impuestas al par�metro.
+	 */
+	public void addLinea(Linea linea) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Obtiene una Linea de la Red mediante la posici�n indicada como par�metro.
+	 *
+	 * @param posicion
+	 *            Entero que indica la posici�n de la Linea a obtener dentro de
+	 *            la Red.Debe ser correcto: Positiva.
+	 * @return La Linea en la posici�n dada de existir Linea en la posici�n,
+	 *         null en caso contrario.
+	 * @throws IllegalArgumentException
+	 *             en caso de incumplir las condiciones impuestas al par�metro.
+	 */
+	public Linea getLinea(int posicion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Elimina una Linea de la Red de autobuses.
+	 *
+	 * @param linea
+	 *            Linea a borrar.Debe ser correcta: No nula y debe estar dentro
+	 *            de la Red.
+	 * @throws IllegalArgumentException
+	 *             en caso de incumplir las condiciones impuestas al par�metro.
+	 */
+	public void removeLinea(Linea linea) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Obtiene un vector de Lineas que tienen Parada cerca de una Direccion y un
+	 * radio dados.
+	 *
+	 * @param direccion
+	 *            DireccionGPS en la que debe haber Paradas cerca.Debe ser
+	 *            correcta: No nula.
+	 * @param radio
+	 *            Radio para buscar Paradas dentro de la Red.Debe ser correcto:
+	 *            Positivo.
+	 * @return Vector con las Lineas que tienen Parada cerca de la Direccion.
+	 * @throws IllegalArgumentException
+	 *             en caso de incumplir alguna de las condiciones impuestas a
+	 *             los par�metros.
+	 */
+	public Linea[] infoParadas(DireccionGPS direccion, int radio) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Obtiene la distancia en metros entre dos Paradas de la Red.
+	 *
+	 * @param parada1
+	 *            Primera Parada a comprobar. Debe ser correcta: No nula.
+	 * @param parada2
+	 *            Segunda Parada a comprobar. Debe ser correcta: No nula.
+	 * @return La distancia en metros entre las 2 Paradas.
+	 * @throws IllegalArgumentException
+	 *             si se incumple alguna de las condiciones impuestas a los
+	 *             par�metros.
+	 */
+	public int getDistanciaParadas(Parada parada1, Parada parada2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
