62f324f71fd06e043691c4c659dadd3f0dbb3459
antroma <antonio.roman@alumnos.uva.es>
2017-12-21 09:24:11 +0100

    Prueba de paso de test

---
+++ src/test/java/uva/tds/pr2/equipo10/Prueba.java
@@ -0,0 +1,14 @@
+package uva.tds.pr2.equipo10;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category({Unit.class})
+public class DireccionGPSTest {
+    @Test
+	public void constructorCorrectoTest() {
+        
+	}
+}
