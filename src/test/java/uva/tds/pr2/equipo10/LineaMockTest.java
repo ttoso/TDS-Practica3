76ab556dfc2fe3a37da90980adb9fb65b645e146
Ivan Gonzalez <ivan@MacBook-Pro-de-Ivan.local>
2017-12-26 23:31:23 +0100

    Isolation test para la clase Linea

--- src/test/java/uva/tds/pr2/equipo10/LineaMockTest.java
+++ src/test/java/uva/tds/pr2/equipo10/LineaMockTest.java
@@ -20,9 +20,6 @@ public class LineaMockTest {
 	private DireccionGPS direccion1;
 	private DireccionGPS direccion2;
 	private DireccionGPS direccion3;
-	private Parada parada1;
-	private Parada parada2;
-	private Parada parada3;
 	private Parada paradas[];
 	private int identificador;
 	private Linea linea;
@@ -30,57 +27,41 @@ public class LineaMockTest {
 	@Mock
 	private Parada mParada1;
 	private Parada mParada2;
-	private Parada mParada3;
-
-	@Before
-	public void setUp() {
-		direccion1 = new DireccionGPS(20.45, 30.50);
-		direccion2 = new DireccionGPS(30.5, 20.4);
-		direccion3 = new DireccionGPS(20.44999, 30.50);
-		parada1 = new Parada(direccion1);
-		parada2 = new Parada(direccion2);
-		parada3 = new Parada(direccion3);
-		paradas = new Parada[3];
-		paradas[0] = parada1;
-		paradas[1] = parada2;
-		paradas[2] = parada3;
-		identificador = 1;
-		linea = new Linea(identificador, paradas);
-	}
-
-	@After
-	public void tearDown() {
-		direccion1 = null;
-		direccion2 = null;
-		direccion3 = null;
-		parada1 = null;
-		parada2 = null;
-		parada3 = null;
-		paradas = null;
-		identificador = 0;
-		linea = null;
-	}
 
 	@Test
 	public void hasCorrespondenciaCorrectoTest() {
 
+		direccion1 = new DireccionGPS(20.45, 30.50);
+		direccion2 = new DireccionGPS(30.5, 20.4);
+		direccion3 = new DireccionGPS(20.44999, 30.50);
+		paradas = new Parada[3];
+		
 		mParada1 = createMock(Parada.class);
 		mParada2 = createMock(Parada.class);
-		mParada3 = createMock(Parada.class);
-
-		expect(mParada1.getDireccion()).andReturn((DireccionGPS) direccion1);
-		expect(mParada2.getDireccion()).andReturn((DireccionGPS) direccion2);
-		expect(mParada3.getDireccion()).andReturn((DireccionGPS) direccion3);
-
+		expect(mParada1.getDistancia(mParada1)).andReturn((double)0).times(2);
+		expect(mParada1.getDistancia(mParada2)).andReturn((double)0).times(2);
+		expect(mParada1.getDireccion()).andReturn((DireccionGPS) direccion1).once();
+		replay(mParada1);
+		replay(mParada2);
+		
+		paradas[0] = mParada1;
+		paradas[1] = mParada1;
+		paradas[2] = mParada1;
+		identificador = 1;
+		linea = new Linea(identificador, paradas);
+		
 		Parada _paradas[] = new Parada[3];
 		_paradas[0] = mParada1;
-		_paradas[1] = mParada2;
-		_paradas[2] = mParada3;
+		_paradas[1] = mParada1;
+		_paradas[2] = mParada1;
 		int _identificador = 2;
 		Linea _linea = new Linea(_identificador, _paradas);
 
 		assertTrue(linea.hasCorrespondencia(_linea));
 		assertNotNull(_linea);
+		verify(mParada1);
+		verify(mParada2);
+
 	}
 
 }
