4c8f6674b101ff143d8b2cb174b392575417bc3d
Iván González Rincón <ivan.gonzalez.rincon@alumnos.uva.es>
2017-12-26 14:38:43 +0100

    Quitado fail clase test DireccionGPS

--- src/test/java/uva/tds/pr2/equipo10/DireccionGPSTest.java
+++ src/test/java/uva/tds/pr2/equipo10/DireccionGPSTest.java
@@ -69,7 +69,6 @@ public class DireccionGPSTest {
 
 	@Test
 	public void secuenciaTest() {
-		fail("Quitar en implementacion");
 		double latitud = 1.0;
 		double longitud = 1.0;
 		DireccionGPS direccion = new DireccionGPS(latitud, longitud);
